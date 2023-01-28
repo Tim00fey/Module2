70c92336d70cf2dfdece7d0eb772d155fd91ccf1
Tim123456Tim <86416015+Tim123456Tim@users.noreply.github.com>
2023-01-28 22:42:18 +0400

    Initial Commit

---
+++ Module2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module2
+//
+//  Created by Тимофей Юдин on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
