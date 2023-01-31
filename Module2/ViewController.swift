d2e899e50f658e96cd12e67895b16f276ca89436
Tim123456Tim <86416015+Tim123456Tim@users.noreply.github.com>
2023-01-31 21:38:45 +0400

    Traffic Light

--- Module2/ViewController.swift
+++ Module2/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum Color: Int{
+        case red = 0
+        case yellow = 1
+        case green = 2
+    }
+    
+    private var color: Color = .red
+    
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var views: [UIView]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 7
+        
+        for view in views {
+            view.layer.cornerRadius = 50
+            view.alpha = 0.3
+        }
+    }
+    
+    private func blackout(_ views: [UIView]!){
+        for view in views {
+            view.alpha = 0.3
+        }
     }
 
-
+    @IBAction func startButtonDidTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch color {
+        case .red:
+            blackout(views)
+            views[color.rawValue].alpha = 1
+            color = .yellow
+        case .yellow:
+            blackout(views)
+            views[color.rawValue].alpha = 1
+            color = .green
+        case .green:
+            blackout(views)
+            views[color.rawValue].alpha = 1
+            color = .red
+        }
+    }
 }
 
